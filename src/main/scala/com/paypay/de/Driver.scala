e1102cc1f04129b0663fb0b6715ca92c2fa88f5a
Lieu Ranjan Gogoi <lieuranjan007@gmail.com>
2019-05-17 20:26:14 +0530

    Create Driver.scala

---
+++ src/main/scala/com/paypay/de/Driver.scala
@@ -0,0 +1,35 @@
+package com.paypay.de
+
+import java.io.File
+
+import org.apache.spark.sql.SparkSession
+
+import scala.math.max
+
+object Driver {
+
+  def main(args: Array[String]): Unit = {
+    val args=Array[String]{"data/2015_07_22_mktplace_shop_web_log_sample_100.log"}
+
+    if(args.length<1){
+      println("USAGE: <input file path>")
+      sys.exit(-1)
+    }
+    val inputDataPath=args(0)
+    //validate input path
+    val inFIle=new File(inputDataPath)
+    if(! inFIle.exists()){
+      throw new RuntimeException(s"Input File $inputDataPath Not Found")
+    }
+    val numOfPartitions=max(inFIle.length()/1000000,10).toInt
+    //initialize the spark session
+    val spark = SparkSession.builder()
+      .appName("DE-Challenge")
+      .master("local[*]")
+      .getOrCreate()
+
+    val runner=new DEChallenge(spark,inputDataPath,numOfPartitions)
+    runner.run()
+  }
+
+}
