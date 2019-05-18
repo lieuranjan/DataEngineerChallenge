659b6c5a25cfa8d38bdb8b03b20a2b7247aabc6a
Lieu Ranjan Gogoi <lieuranjan007@gmail.com>
2019-05-18 14:37:25 +0530

    All test code done.

--- src/main/scala/com/paypay/de/Driver.scala
+++ src/main/scala/com/paypay/de/Driver.scala
@@ -4,12 +4,12 @@ import java.io.File
 
 import org.apache.spark.sql.SparkSession
 
-import scala.math.max
+import scala.math.min
 
 object Driver {
 
   def main(args: Array[String]): Unit = {
-    val args=Array[String]{"data/2015_07_22_mktplace_shop_web_log_sample_100.log"}
+    val args=Array[String]{"data/2015_07_22_mktplace_shop_web_log_sample.log.gz"}
 
     if(args.length<1){
       println("USAGE: <input file path>")
@@ -21,7 +21,9 @@ object Driver {
     if(! inFIle.exists()){
       throw new RuntimeException(s"Input File $inputDataPath Not Found")
     }
-    val numOfPartitions=max(inFIle.length()/1000000,10).toInt
+    println(inFIle.length())
+    val numOfPartitions=min(inFIle.length()/10000000,10).toInt
+    println(numOfPartitions)
     //initialize the spark session
     val spark = SparkSession.builder()
       .appName("DE-Challenge")
